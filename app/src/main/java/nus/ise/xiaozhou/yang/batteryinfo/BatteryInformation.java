841893f53cfdbbfb2b79c6f6337566ab9717d7e9
Yang_Xiaozhou <yang_xiaozhou@icloud.com>
2015-09-15 20:22:56 +0800

    display text minor change

--- app/src/main/java/nus/ise/xiaozhou/yang/batteryinfo/BatteryInformation.java
+++ app/src/main/java/nus/ise/xiaozhou/yang/batteryinfo/BatteryInformation.java
@@ -29,30 +29,32 @@ public class BatteryInformation extends Activity {
         @Override
         public void onReceive(Context context, Intent intent) {
 
-            int  health= intent.getIntExtra(BatteryManager.EXTRA_HEALTH,0);
-            int  icon_small= intent.getIntExtra(BatteryManager.EXTRA_ICON_SMALL,0);
-            int  level= intent.getIntExtra(BatteryManager.EXTRA_LEVEL,0);
-            int  plugged= intent.getIntExtra(BatteryManager.EXTRA_PLUGGED,0);
-            boolean  present= intent.getExtras().getBoolean(BatteryManager.EXTRA_PRESENT);
-            int  scale= intent.getIntExtra(BatteryManager.EXTRA_SCALE,0);
-            int  status= intent.getIntExtra(BatteryManager.EXTRA_STATUS,0);
-            String  technology= intent.getExtras().getString(BatteryManager.EXTRA_TECHNOLOGY);
-            int  temperature= intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE,0);
-            int  voltage= intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE,0);
+            int  health = intent.getIntExtra(BatteryManager.EXTRA_HEALTH,0);
+            int  icon_small = intent.getIntExtra(BatteryManager.EXTRA_ICON_SMALL,0);
+            int  level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            boolean  plugged = intent.getExtras().getBoolean(BatteryManager.EXTRA_PLUGGED);
+            boolean  present = intent.getExtras().getBoolean(BatteryManager.EXTRA_PRESENT);
+            int  scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, 0);
+            int  status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, 0);
+            String  technology = intent.getExtras().getString(BatteryManager.EXTRA_TECHNOLOGY);
+            int  temperature = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0);
+            int  voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0);
+
 
 
             batteryInfo.setText(
-                    "Health: "+health+"\n"+
-                            "Icon Small:"+icon_small+"\n"+
-                            "Remaining Battery: "+level+ "%"+"\n"+
-                            "Plugged: "+plugged+"\n"+
-                            "Present: "+present+"\n"+
-                            "Scale: "+scale+"\n"+
-                            "Status: "+status+"\n"+
-                            "Battery Technology: "+technology+"\n"+
-                            "Temperature: "+temperature+"\n"+
-                            "Voltage: "+voltage+"\n");
-            // imageBatteryState.setImageResource(icon_small);
+                    "Health: " + health + "\n" +
+                            "Remaining Battery: " + level + "%" + "\n" +
+                            // "Phone Plugged?: " + plugged + "\n" +
+                            "Battery Present?: " + present + "\n" +
+                            "Scale: " + scale + "\n" +
+                            "Status: " + status + "\n" +
+                            "Battery Technology: " + technology + "\n" +
+                            "Temperature: " + temperature + "\n" +
+                            "Voltage: " + voltage + "\n");
+            imageBatteryState.setImageResource(icon_small);
+            imageBatteryState.setScaleType(ImageView.ScaleType.FIT_CENTER);
+
         }
     };
 
