f1e34d644ea77d69f45ec4a954954fe86c03c269
Yang_Xiaozhou <yang_xiaozhou@icloud.com>
2015-09-29 09:44:59 +0800

    update battery info presentation

--- app/src/main/java/nus/ise/xiaozhou/yang/batteryinfo/BatteryInformation.java
+++ app/src/main/java/nus/ise/xiaozhou/yang/batteryinfo/BatteryInformation.java
@@ -28,38 +28,99 @@ public class BatteryInformation extends Activity {
     private BroadcastReceiver batteryInfoReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-
-            int  health = intent.getIntExtra(BatteryManager.EXTRA_HEALTH,0);
-            int  icon_small = intent.getIntExtra(BatteryManager.EXTRA_ICON_SMALL,0);
-            int  level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
-            boolean  plugged = intent.getExtras().getBoolean(BatteryManager.EXTRA_PLUGGED);
+            final String DEGREE  = "\u2103";
             boolean  present = intent.getExtras().getBoolean(BatteryManager.EXTRA_PRESENT);
-            int  scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, 0);
-            int  status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, 0);
-            String  technology = intent.getExtras().getString(BatteryManager.EXTRA_TECHNOLOGY);
-            int  temperature = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0);
-            temperature = temperature - 273;
-            int  voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0);
+            if(present) {
+                Bundle bundle = intent.getExtras();
+                int health = intent.getIntExtra(BatteryManager.EXTRA_HEALTH, 0);
+                int icon_small = intent.getIntExtra(BatteryManager.EXTRA_ICON_SMALL, 0);
+                int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+                int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, 100);
+                int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, 0);
+                int temperature = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0) / 10; //convert to celcius degrees
+                int voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0);
+                int current = bundle.getInt("current_avg");
+                int plugType = bundle.getInt(BatteryManager.EXTRA_PLUGGED);
+                String technology = bundle.getString(BatteryManager.EXTRA_TECHNOLOGY);
 
 
+                batteryInfo.setText(
+                        "Remaining Battery: " + level + "%" + "\n" +
+                                "Scale: " + scale + "\n" +
+                                "Battery Technology: " + technology + "\n" +
+                                "Temperature: " + temperature + " " + DEGREE + "\n" +
+                                "Voltage: " + voltage + " mV" + "\n" +
+                                "Current: " + current + " mA" + "\n" +
+                                "Health: " + getHealthString(health) + "\n" +
+                                "Charging State: " + getStatusString(status) + " (" + getPlugTypeString(plugType) + ")"
+                );
 
-            batteryInfo.setText(
-                    "Health: " + health + "\n" +
-                            "Remaining Battery: " + level + "%" + "\n" +
-                            // "Phone Plugged?: " + plugged + "\n" +
-                            "Battery Present? " + present + "\n" +
-                            "Scale: " + scale + "\n" +
-                            "Status: " + status + "\n" +
-                            "Battery Technology: " + technology + "\n" +
-                            "Temperature: " + temperature + " Celsius Degrees" + "\n" +
-                            "Voltage: " + voltage + " millivolt" + "\n");
+                imageBatteryState.setImageResource(icon_small);
+                imageBatteryState.setScaleX(5);
+                imageBatteryState.setScaleY(5);
+                imageBatteryState.setPadding(500, 500, 500, 500);
 
-            imageBatteryState.setImageResource(icon_small);
-            imageBatteryState.setScaleX(5);
-            imageBatteryState.setScaleY(5);
-            imageBatteryState.setPadding(100,100,100,100);
+            }else{
+                batteryInfo.setText("Battery is not present!");
+            }
+
         }
     };
+
+    private String getPlugTypeString(int plugged) {
+        String plugType = "Unknown";
+
+        switch (plugged) {
+            case BatteryManager.BATTERY_PLUGGED_AC:
+                plugType = "AC";
+                break;
+            case BatteryManager.BATTERY_PLUGGED_USB:
+                plugType = "USB";
+                break;
+        }
+        return plugType;
+    }
+
+    private String getHealthString(int health) {
+        String healthString = "Unknown";
+        switch (health) {
+            case BatteryManager.BATTERY_HEALTH_DEAD:
+                healthString = "Dead";
+                break;
+            case BatteryManager.BATTERY_HEALTH_GOOD:
+                healthString = "Good Condition";
+                break;
+            case BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE:
+                healthString = "Over Voltage";
+                break;
+            case BatteryManager.BATTERY_HEALTH_OVERHEAT:
+                healthString = "Over Heat";
+                break;
+            case BatteryManager.BATTERY_HEALTH_UNSPECIFIED_FAILURE:
+                healthString = "Failure";
+                break;
+        }
+        return healthString;
+    }
+    private String getStatusString(int status) {
+        String statusString = "Unknown";
+
+        switch (status) {
+            case BatteryManager.BATTERY_STATUS_CHARGING:
+                statusString = "Charging";
+                break;
+            case BatteryManager.BATTERY_STATUS_DISCHARGING:
+                statusString = "Discharging";
+                break;
+            case BatteryManager.BATTERY_STATUS_FULL:
+                statusString = "Full";
+                break;
+            case BatteryManager.BATTERY_STATUS_NOT_CHARGING:
+                statusString = "Not Charging";
+                break;
+        }
+        return statusString;
+    }
 
 
 }
