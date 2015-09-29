0a06cb1fa54d523c47a2d2bae50331080f850e81
Yang_Xiaozhou <yang_xiaozhou@icloud.com>
2015-09-29 20:14:38 +0800

    include more project files, update mainActivity UI, build UI template

--- app/src/main/java/nus/ise/xiaozhou/yang/batteryinfo/BatteryInformation.java
+++ app/src/main/java/nus/ise/xiaozhou/yang/batteryinfo/BatteryInformation.java
@@ -12,60 +12,93 @@ import android.widget.TextView;
 
 
 public class BatteryInformation extends Activity {
-    private TextView batteryInfo;
-    private ImageView imageBatteryState;
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        batteryInfo=(TextView)findViewById(R.id.textViewBatteryInfo);
-        imageBatteryState=(ImageView)findViewById(R.id.imageViewBatteryState);
-
-        this.registerReceiver(this.batteryInfoReceiver,	new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-    }
-
+    private TextView remainingTime, remainingPercentage, remainingPercentageData, batteryTechnology, batteryTechnologyData,
+            batteryTemperature, batteryTemperatureData, batteryVoltage, batteryVoltageData,
+            batteryHealth, batteryHealthData, batteryChargingState, batteryChargingStateData;
     private BroadcastReceiver batteryInfoReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            final String DEGREE  = "\u2103";
-            boolean  present = intent.getExtras().getBoolean(BatteryManager.EXTRA_PRESENT);
-            if(present) {
+            final String DEGREE = "\u2103";
+            boolean present = intent.getExtras().getBoolean(BatteryManager.EXTRA_PRESENT);
+            if (present) {
                 Bundle bundle = intent.getExtras();
                 int health = intent.getIntExtra(BatteryManager.EXTRA_HEALTH, 0);
                 int icon_small = intent.getIntExtra(BatteryManager.EXTRA_ICON_SMALL, 0);
                 int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
                 int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, 100);
                 int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, 0);
-                int temperature = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0) / 10; //convert to celcius degrees
+                int temperature = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0) / 10; //convert to Celcius Degrees
                 int voltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0);
-                int current = bundle.getInt("current_avg");
                 int plugType = bundle.getInt(BatteryManager.EXTRA_PLUGGED);
                 String technology = bundle.getString(BatteryManager.EXTRA_TECHNOLOGY);
 
 
-                batteryInfo.setText(
+                remainingTime.setText("REMAINING TIME: 6 Hrs 54 Mins");
+
+                remainingPercentage.setText("Remaining Battery:");
+                remainingPercentageData.setText(level + "%");
+
+                batteryTechnology.setText("Battery Technology:");
+                batteryTechnologyData.setText(technology);
+
+                batteryTemperature.setText("Temperature:");
+                batteryTemperatureData.setText(temperature + " " + DEGREE);
+
+                batteryVoltage.setText("Voltage:");
+                batteryVoltageData.setText(voltage + " mV");
+
+                batteryHealth.setText("Health:");
+                batteryHealthData.setText(getHealthString(health));
+
+                batteryChargingState.setText("Charging State:");
+                batteryChargingStateData.setText(getStatusString(status) + " (" + getPlugTypeString(plugType) + ")");
+
+/*                batteryInfo.setText(
                         "Remaining Battery: " + level + "%" + "\n" +
-                                "Scale: " + scale + "\n" +
                                 "Battery Technology: " + technology + "\n" +
                                 "Temperature: " + temperature + " " + DEGREE + "\n" +
                                 "Voltage: " + voltage + " mV" + "\n" +
-                                "Current: " + current + " mA" + "\n" +
                                 "Health: " + getHealthString(health) + "\n" +
                                 "Charging State: " + getStatusString(status) + " (" + getPlugTypeString(plugType) + ")"
-                );
+                );*/
 
-                imageBatteryState.setImageResource(icon_small);
+/*                imageBatteryState.setImageResource(icon_small);
                 imageBatteryState.setScaleX(5);
                 imageBatteryState.setScaleY(5);
-                imageBatteryState.setPadding(500, 500, 500, 500);
+                imageBatteryState.setPadding(500, 500, 500, 500);*/
 
-            }else{
-                batteryInfo.setText("Battery is not present!");
+            } else {
+                remainingTime.setText("Battery is not present!");
             }
 
         }
     };
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        //Find all textviews in the mainActivity
+        remainingTime = (TextView) findViewById(R.id.remainingtime);
+        remainingPercentage = (TextView) findViewById(R.id.remainingpercentage);
+        remainingPercentageData = (TextView) findViewById(R.id.remainingpercentagedata);
+        batteryTechnology = (TextView) findViewById(R.id.batterytechnology);
+        batteryTechnologyData = (TextView) findViewById(R.id.batterytechnologydata);
+        batteryTemperature = (TextView) findViewById(R.id.temperature);
+        batteryTemperatureData = (TextView) findViewById(R.id.temperaturedata);
+        batteryVoltage = (TextView) findViewById(R.id.voltage);
+        batteryVoltageData = (TextView) findViewById(R.id.voltagedata);
+        batteryHealth = (TextView) findViewById(R.id.health);
+        batteryHealthData = (TextView) findViewById(R.id.healthdata);
+        batteryChargingState = (TextView) findViewById(R.id.chargingstate);
+        batteryChargingStateData = (TextView) findViewById(R.id.charingstatedata);
+
+        this.registerReceiver(this.batteryInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    }
 
     private String getPlugTypeString(int plugged) {
         String plugType = "Unknown";
@@ -102,6 +135,7 @@ public class BatteryInformation extends Activity {
         }
         return healthString;
     }
+
     private String getStatusString(int status) {
         String statusString = "Unknown";
 
